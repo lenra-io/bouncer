a5a5c6492658d7ce386eb562ec05a3c8f0152a44
Louis <louis@lenra.me>
2021-07-01 17:32:03 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Bouncer
+
+**TODO: Add description**
+
+## Installation
+
+If [available in Hex](https://hex.pm/docs/publish), the package can be installed
+by adding `bouncer` to your list of dependencies in `mix.exs`:
+
+```elixir
+def deps do
+  [
+    {:bouncer, "~> 0.1.0"}
+  ]
+end
+```
+
+Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
+and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
+be found at [https://hexdocs.pm/bouncer](https://hexdocs.pm/bouncer).
+
---
+++ mix.exs
@@ -0,0 +1,29 @@
+defmodule Bouncer.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :bouncer,
+      version: "0.2.0",
+      elixir: "~> 1.12",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:ex_doc, "~> 0.22", only: :dev, runtime: false},
+      {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
+      {:credo, "~> 1.4", only: [:dev, :test], runtime: false}
+    ]
+  end
+end
