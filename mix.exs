efac0499552dd6669bdce1c3592cf4bd8e2caa70
Louis G <louis@lenra.me>
2021-08-17 13:01:38 +0200

    ci: Remove commit to push verison on mix.exs and README.md (#20)

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Bouncer.MixProject do
   def project do
     [
       app: :bouncer,
-      version: "1.0.0-beta.2",
+      version: "0.0.0-dev.1",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps()
